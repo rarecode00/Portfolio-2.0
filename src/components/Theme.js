2ebf41b44c2ff955b86926b9fb44f13843eb3745
Krishna Sharma <Ks9192174414@gmail.com>
2023-03-18 16:49:55 +0530

    themes added

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 function Theme() {
+  const location = useLocation();
+  const [show , setShow] = useState(true);
+
+  const handleColor = (color) =>{
+     document.documentElement.style.setProperty('--const-color' , color);    
+  }
 
   return (
     <div className="theme">
-     theme
-     <div className='theme-options'>
-        <div className='option color1'></div>
-        <div className='option color2'></div>
-        <div className='option color3'></div>
+     <div className={`theme-options`} style = {{display:location.pathname === '/' ? '' : 'none'}}>
+        <div className='option color1' onClick={() =>{handleColor('green')}}></div>
+        <div className='option color2' onClick={() =>{handleColor('red')}}></div>
+        <div className='option color3' onClick={() =>{handleColor('blueviolet')}}></div>
      </div>
     </div>
   );
